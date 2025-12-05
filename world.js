03752082f4bd80f71d472dd37107f900eb8275f6
YoriichiTsugiikuni <romainedixon66@gmail.com>
2025-12-05 12:25:40 -0500

    Exercise 5 completed

--- world.js
+++ world.js
@@ -1,21 +1,38 @@
 window.onload = function() {
-    var lookupButton = document.getElementById('lookup');
-    var countryInput = document.getElementById('country');
-    var resultDiv = document.getElementById('result');
+    var countryLookup = document.getElementById('lookup');
+    var citiesLookup = document.getElementById('lookup-city');
+    var searchInput = document.getElementById('country');
+    var outputArea = document.getElementById('result');
 
-    lookupButton.addEventListener('click', function() {
-        var country = countryInput.value;
-        var url = 'world.php?country=' + encodeURIComponent(country);
+    countryLookup.addEventListener('click', function() {
+        var searchTerm = searchInput.value;
+        var requestUrl = 'world.php?country=' + encodeURIComponent(searchTerm);
 
-        var xhr = new XMLHttpRequest();
+        var request = new XMLHttpRequest();
 
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                resultDiv.innerHTML = xhr.responseText;
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                outputArea.innerHTML = request.responseText;
             }
         };
 
-        xhr.open('GET', url, true);
-        xhr.send();
+        request.open('GET', requestUrl, true);
+        request.send();
+    });
+
+    citiesLookup.addEventListener('click', function() {
+        var searchTerm = searchInput.value;
+        var requestUrl = 'world.php?country=' + encodeURIComponent(searchTerm) + '&lookup=cities';
+
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                outputArea.innerHTML = request.responseText;
+            }
+        };
+
+        request.open('GET', requestUrl, true);
+        request.send();
     });
 };
--- world.php
+++ world.php
@@ -6,15 +6,47 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-$country = $_GET['country'];
+$searchTerm = $_GET['country'];
+$queryType = isset($_GET['lookup']) ? $_GET['lookup'] : '';
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+if ($queryType === 'cities') {
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$searchTerm%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th>
+  </tr>
+<?php foreach ($results as $record): ?>
+  <tr>
+    <td><?= $record['name']; ?></td>
+    <td><?= $record['district']; ?></td>
+    <td><?= $record['population']; ?></td>
+  </tr>
 <?php endforeach; ?>
-</ul>
+</table>
+<?php
+} else {
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$searchTerm%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
+<?php foreach ($results as $record): ?>
+  <tr>
+    <td><?= $record['name']; ?></td>
+    <td><?= $record['continent']; ?></td>
+    <td><?= $record['independence_year']; ?></td>
+    <td><?= $record['head_of_state']; ?></td>
+  </tr>
+<?php endforeach; ?>
+</table>
+<?php
+}
